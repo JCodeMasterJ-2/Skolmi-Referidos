a9ce9cce15ea7f28499977a9c58443b547787a34
bycris13 <cristianariasu13@gmail.com>
2024-12-12 18:03:04 -0500

    Avances en la DB
    Casi pero no tanto

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const port = 3019
+
+const app = express()
+app.use(express.static(__dirname))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'form.html'))
+})
+app.listen(port,()=>{
+    console.log(`Server running at http://localhost:${port}`)
+})
